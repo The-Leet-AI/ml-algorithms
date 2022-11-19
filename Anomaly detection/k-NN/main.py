629a1f3c40e14023559eb73e08d4b545b8b1a7f1
Ayoub Achak <ayoub.achak01@gmail.com>
2022-11-19 03:30:03 +0100

    added k-nn for anomaly detection

---
+++ Anomaly detection/k-NN/main.py
@@ -0,0 +1,36 @@
+from time import time
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.model_selection import cross_val_score
+
+""" Runs the K-Nearest Neighbors Classifier """
+def run(data, k_nearest):
+    print(">>>>> K-NEAREST NEIGHBORS (k=" + str(k_nearest) +") <<<<<")
+    start = time()
+    np.random.shuffle(data)
+
+    classes = data[:, 57] # Classification for each email in the dataset
+    features = data[:, :54] # Features for each email in the dataset
+
+    clf = KNeighborsClassifier(n_neighbors=k_nearest, p=2, metric="euclidean")
+    results = cross_val_score(clf, features, classes, cv=10, n_jobs=-1)
+    print("--------------------------")
+    print("Accuracy (minimum): %.3f%%" % (results.min() * 100))
+    print("Accuracy (maximum): %.3f%%" % (results.max() * 100))
+    print("Accuracy (mean): %.3f%%" % (results.mean() * 100))
+    print("Variance: " + str(results.var()))
+
+    end = time()
+    print("\nTime elapsed: {}".format(end - start))
+    print("--------------------------\n")
+
+def main ():
+    # Load dataset
+    filename = "data/spambase/spambase.data"
+    file = open(filename, "r")
+    dataset = np.loadtxt(file, delimiter = ",")
+    k_nearest = 5
+
+    run(dataset, k_nearest)
+if __name__ == '__main__':
+    main()
